b8608c753c495fe8614a05f0ed6b3b8c53b331f0
ANURAG <78607261+ma-anand@users.noreply.github.com>
2024-02-26 18:47:10 +0000

    #

--- apiCall.py
+++ apiCall.py
@@ -8,3 +8,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+#Downloading StopWords
+import nltk
+nltk.download('stopwords')
+#print(stopwords.words('english'))
+
+
+#Data Processing
+
+#loading the data from csv file to pandas dataframe
+user_data=pd.read_csv('fileName.csv', encoding= 'ISO-8859-1')
+
+user_data.shape
