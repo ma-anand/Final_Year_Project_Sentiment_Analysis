7d50859d30146bb9756c3277f8bb805d89d32f04
ANURAG <78607261+ma-anand@users.noreply.github.com>
2024-02-26 18:38:27 +0000

    install Library

---
+++ a.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import re
+import pickle
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
